2712ca42fa5ce5da29b7d5cfe7706d3230f10560
brahim1205 <sadiocheri11@gmail.com>
2025-06-11 20:10:22 +0000

    premier commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://project-ochre-nine-50.vercel.app',
-    //'https://project-git-main-sadiocherif11-gmailcoms-projects.vercel.app'
+    'https://project-git-main-sadiocherif11-gmailcoms-projects.vercel.app'
   ]
 }));
 
